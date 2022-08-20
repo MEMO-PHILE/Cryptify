6dd1d0c56f6371fdf6a124fb7c9d02da393ebfb3
MEMO-PHILE <pjdrd24680@gmail.com>
2022-08-20 13:49:57 +0530

    Cryptify App

---
+++ app/src/main/java/com/memonic/cryptify/MainActivity.java
@@ -0,0 +1,29 @@
+package com.memonic.cryptify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageEncrypt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        imageEncrypt = findViewById(R.id.img_encrypt);
+        imageEncrypt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent m2 = new Intent(MainActivity.this,PhotoCrypto.class);
+                startActivity(m2);
+            }
+        });
+    }
+}
